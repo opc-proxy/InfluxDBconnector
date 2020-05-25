d717ed183484915861bb7bafb2eade574c843ccd
panManfredini <pan.manfredini@gmail.com>
2020-05-25 19:30:37 +0200

    moved to influxDB 2.0

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,14 +1,50 @@
 using System;
 using Xunit;
+using OpcInfluxConnect;
+using OpcProxyCore;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Test
 {
     public class UnitTest1
     {
+        serviceManager sm;
+        OpcInflux influx;
+        public UnitTest1()
+        {
+            var json = JObject.Parse(@"
+                {
+                    'opcServerURL':'opc.tcp://localhost:4840/freeopcua/server/',
+                    'loggerConfig' :{
+                        'loglevel' :'debug'
+                    },
+
+                    'nodesLoader' : {
+                        'targetIdentifier' : 'browseName',
+                        'whiteList':['MyVariable']
+                    },
+                    'influx':{
+                        'organizationName' : 'dsi',
+                        'bucketName' : 'peppo4' 
+                    }
+                }
+            ");
+
+            Environment.SetEnvironmentVariable("OPC_INFLUX_TOKEN","6_slTQpvMu67fhqOxBIlS9WTJGkadjHvaYhgvls95POrtVA9m48yZuKNyr5NidnA8P9Fd4hRB6ZoCqLZqXd5GQ==");
+            //sm = new serviceManager(new string[] {""});
+            sm = new serviceManager(json);
+            influx = new OpcInflux();
+            sm.addConnector(influx);
+            Task.Run( () => sm.run() );
+            Console.WriteLine("Warming up...");
+            Thread.Sleep(1000);
+        }
         [Fact]
         public void Test1()
         {
-
+            Thread.Sleep(5000);
         }
     }
 }
--- opcInfluxConnect/InfluxConnect.cs
+++ opcInfluxConnect/InfluxConnect.cs
@@ -1,58 +1,120 @@
 using System;
-using OpcProxyCore;
-using Newtonsoft.Json.Linq;
 using System.Threading;
-using InfluxDB.Collector;
-using OpcProxyClient;
-
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Core;
+using InfluxDB.Client.Writes;
+using Newtonsoft.Json.Linq;
 using NLog;
+using Opc.Ua;
+using OpcProxyClient;
+using OpcProxyCore;
 
-namespace OpcInfluxConnect{
-    public class InfluxImpl : IOPCconnect {
+namespace OpcInfluxConnect {
+    public class OpcInflux : IOPCconnect {
         serviceManager _serv;
-        public static Logger logger = LogManager.GetCurrentClassLogger();
-        public void setServiceManager(serviceManager serv){
+        public static Logger logger = LogManager.GetCurrentClassLogger ();
+
+        public InfluxDBClient client = null;
+        public WriteApi writeApi;
+        public InfluxConfigWrapper _conf;
+
+        public void setServiceManager (serviceManager serv) {
             _serv = serv;
         }
-        public void OnNotification(object sub, MonItemNotificationArgs items){
+        public void OnNotification (object sub, MonItemNotificationArgs items) {
 
-            foreach(var itm in items.values){
-                Metrics.Collector.Measure(items.name,itm.Value );
+            foreach (var itm in items.values) {
+                if (DataValue.IsBad (itm)) continue;
+                dynamic value = itm.Value;
+                var point = PointData.Measurement(_conf.opcSystemName)
+                    .Tag ("Name", items.name)
+                    .Field ("value", value)
+                    .Timestamp (itm.SourceTimestamp.ToUniversalTime (), WritePrecision.Ms);
+
+                writeApi.WritePoint(point);
+
+                logger.Debug("Write to influxDB value for {0} val {1}", items.name, itm.Value.ToString());
+            }
+        }
+        public async void init (JObject config, CancellationTokenSource cts) {
+
+            try {
+                _conf = config.ToObject<InfluxConfigWrapper>();
+                string Url = "http://"+_conf.influx.host + ":" + _conf.influx.port.ToString();
+                string token = Environment.GetEnvironmentVariable("OPC_INFLUX_TOKEN") ?? "";
+
+                var client_opt = new InfluxDBClientOptions.Builder()
+                    .Url(Url)
+                    .AuthenticateToken(token.ToCharArray())
+                    .Bucket(_conf.influx.bucketName)
+                    .Org(_conf.influx.organizationName)
+                    .ReadWriteTimeOut(TimeSpan.FromMilliseconds(_conf.influx.timeoutMs))
+                    .TimeOut(TimeSpan.FromMilliseconds(_conf.influx.timeoutMs))
+                    .Build();
+
+                client = InfluxDBClientFactory.Create( client_opt );
+                var health = await client.HealthAsync ();
+                if (health.Status != HealthCheck.StatusEnum.Pass) throw new Exception ("Connection failed with host: " + Url);
+
+                // query to test connection... is the only way I could find
+                // Note: the following lines will throw in case
+                var flux = $"from(bucket:\"{_conf.influx.bucketName}\") |> range(start: 0) |> limit(n: 1)";
+                var fluxTables = await client.GetQueryApi().QueryAsync(flux);
                 
-                /* --- This does not work ---- FIXME! 
+                var write_opt = new WriteOptions.Builder()
+                    .BatchSize(_conf.influx.batchSize)
+                    .FlushInterval(_conf.influx.flushIntervalMs)
+                    .RetryInterval(_conf.influx.retryIntervalMs)
+                    .Build();
+                writeApi = client.GetWriteApi(write_opt);
 
-                Metrics.Collector.Write(
-                    items.name, 
-                    new Dictionary<string, object> { { "value", itm.Value } },
-                    null,
-                    DateTime.Now
-                );
-                */
+                logger.Info ("InfluxDB connection succesfully initialized");
 
-                logger.Debug("Write to influxDB value for {0} to {1} at {2}", items.name, itm.Value, itm.SourceTimestamp);
+            } catch (Exception e) {
+                cts.Cancel ();
+                logger.Fatal ("Problems in initializing connection to InfluxDB. Quiting...");
+                logger.Error (e.Message);
             }
         }
 
-        public void init(JObject config, CancellationTokenSource cts){
-            
-            try{
-                Metrics.Collector = new CollectorConfiguration()
-                    //.Tag.With("host", Environment.GetEnvironmentVariable("COMPUTERNAME"))
-                    .Batch.AtInterval(TimeSpan.FromSeconds(5))
-                    .WriteTo.InfluxDB("http://localhost:8086", "opcProxyData")
-                    .CreateCollector();
-                logger.Debug("OpcInfluxConnect initialized");
-            }
-            catch(Exception e){
-                logger.Error(e, "Problems in connecting to Influx DB");
-            }
+        public void clean () {
+            writeApi?.Dispose ();
+            client?.Dispose ();
+            logger.Info ("Influx DB connection closed");
         }
 
-        public void clean(){
-            Metrics.Close();
-            logger.Debug("Influx DB connection closed");
+    }
+
+    public class InfluxConfigWrapper{
+        public InfluxConfig influx {get;set;}
+        public string opcSystemName {get;set;}
+        public InfluxConfigWrapper()
+        {
+            influx = new InfluxConfig();
+            opcSystemName = "OPC";
         }
-        
+    }
+    public class InfluxConfig {
+        public string organizationName {get;set;}
+        public string bucketName {get;set;}
+        public string host {get;set;}
+        public int port {get;set;}
+        public double timeoutMs {get;set;}
+        public int batchSize {get; set;}
+        public int flushIntervalMs {get;set;}
+        public int retryIntervalMs {get; set;}
+        public InfluxConfig(){
+
+            organizationName = "none";
+            bucketName = "";
+            host = "localhost";
+            port = 9999;
+            timeoutMs = 10000;
+            batchSize = 1000;
+            flushIntervalMs = 5000;
+            retryIntervalMs = 1000;
+        }
     }
 
 }
